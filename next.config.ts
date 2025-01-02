2ffe239964ec987f42da542e30687cdc3c3f8433
Vitor <99208505+thenextbunny@users.noreply.github.com>
2025-01-02 15:04:01 -0300

    remoção de arquivos de mkcert

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
-import fs from 'fs';
-import path from "path";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  webpack(config, {isServer}){
-    if(!isServer){
-      config.devServer = {
-        https:{
-          key: fs.readFileSync(path.resolve('localhost-key.pem')),
-          cert: fs.readFileSync(path.resolve('localhost.pem')),
-        },
-      };
-    }
-
-    return config;
-  },
 };
 
 export default nextConfig;
